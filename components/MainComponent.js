1db070627b5a7dda8272e3321653fae106856a20
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-06-16 22:46:16 +0530

    dish details

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import {View } from 'react-native';
 import {DISHES} from '../shared/dishes';
 import Menu from './Menu';
-
+import DishDetails from './DishDetails';
 export default class MainComponent extends React.Component{
  constructor(props){
      super(props);
      this.state = {
-         dishes : DISHES
+         dishes : DISHES,
+         selectedDish:null
      }
  }
 
+ onDishSelected(dishId){
+     this.setState({selectedDish:dishId})
+ }
  render(){
      return(
-        <Menu dishes={this.state.dishes}></Menu>
+         <View style={{flex:1}}>
+            <Menu dishes={this.state.dishes}
+            onPress={dishId => this.onDishSelected(dishId)}
+            />
+            <DishDetails  dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        </View>
      );
  }
 }
--- components/Menu.js
+++ components/Menu.js
@@ -9,6 +9,7 @@ function Menu(props){
             key={index}
             title={item.name}
             subtitle={item.description}
+            onPress={()=> props.onPress(item.id)}
             hideChevron={true}
             leftAvatar={{source: require('./images/uthappizza.png')}}
             />
