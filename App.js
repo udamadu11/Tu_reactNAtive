95a6744e6ed10efd89022aca3af5ac23785e4a71
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-06-16 21:34:20 +0530

    initialize

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <View></View>
+    )
+  }
+}
+
